59e214f5cbe940a840ac79b5afcd8ef18705f98a
Flavio Muscente <flaviomuscente@libero.it>
2021-01-25 18:55:12 +0100

    add function return array

--- app/Http/Controllers/DischiController.php
+++ app/Http/Controllers/DischiController.php
@@ -4,20 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+include app_path() . '\mock\dischi-array.php';
+
+
 class DischiController extends Controller
 {
     public function index(){
-
-        $data_path = (app_path() . '/mock/dischi-array.php');
         
-        $datareplaced_path = str_replace('\\', '/', $data_path);
-        
-        include $datareplaced_path;
-        
-        $data= $music_array['response'];
+        $data= GetArray();
 
 
    return view('dischi', compact('data'));
 
    }
 }
+
+?>
